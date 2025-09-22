ac226dc5fdb7a579a6f3d78ec0d5c4bc8807f1ac
youngers2006 <sy2224@ic.ac.uk>
2025-09-22 15:53:45 +0100

    added

--- WorldModel.py
+++ WorldModel.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from VariationalAutoEncoder import Decoder, Encoder
+from Agent import Actor, Critic
 
 class WorldModel(nn.Module):
     def __init__(self, hidden_dims, latent_dims, device='cpu'):
@@ -11,6 +12,6 @@ class WorldModel(nn.Module):
         self.GRU = nn.GRU(input_size=latent_dims, hidden_size=hidden_dims, device=device)
         self.encoder = Encoder()
         self.decoder = Decoder()
-        self.policy_base = Policy()
+        self.actor = Actor()
+        self.critic = Critic()
     
-    def 
