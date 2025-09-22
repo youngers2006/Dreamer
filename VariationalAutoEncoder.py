e91587d32274cb47fdcc7bb3b9f9529e8807644b
youngers2006 <sy2224@ic.ac.uk>
2025-09-22 22:52:57 +0100

    added

--- VariationalAutoEncoder.py
+++ VariationalAutoEncoder.py
@@ -2,7 +2,42 @@ import torch
 import torch.nn as nn
 
 class Encoder(nn.Module):
-    pass
+    def __init__(self, num_filters_1, num_filters_2, latent_size, device='cpu'):
+        super().__init__()
+        self.feature_extractor = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=num_filters_1, kernel_size=3, stride=1, padding=1, device=device),
+            nn.SiLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(in_channels=num_filters_1, out_channels=num_filters_2, kernel_size=3, stride=1, padding=1, device=device),
+            nn.SiLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.latent_mapper = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=num_filters_2 * 2 * 2, out_features=512, device=device),
+            nn.SiLU(),
+            nn.Linear(in_features=512, out_features=latent_size, device=self.device)
+        )
+    def forward(self, x):
+        x = self.feature_extractor(x)
+        return self.latent_mapper(x)
 
 class Decoder(nn.Module):
-    pass
+    def __init__(self, latent_dim, num_filters_1, num_filters_2, hidden_dim=512, device='cpu'):
+        super().__init__()
+        self.upscaler = nn.Sequential(
+            nn.Linear(in_features=latent_dim, out_features=hidden_dim),
+            nn.SiLU(),
+            nn.Linear(in_features=hidden_dim, out_features=num_filters_2 * 8 * 8),
+            nn.SiLU()
+        )
+        self.image_builder = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=num_filters_2, out_channels=num_filters_1, kernel_size=4, stride=2, padding=1),
+            nn.SiLU(),
+            nn.ConvTranspose2d(in_channels=num_filters_1, out_channels=3, kernel_size=4, stride=2, padding=1),
+            nn.Sigmoid()
+        )
+    def forward(self, x):
+        x = self.upscaler(x)
+        return self.image_builder(x)
+
--- WorldModel.py
+++ WorldModel.py
@@ -1,17 +1,22 @@
 import torch
 import torch.nn as nn
 from VariationalAutoEncoder import Decoder, Encoder
-from Agent import Actor, Critic
 
 class WorldModel(nn.Module):
-    def __init__(self, hidden_dims, latent_dims, device='cpu'):
+    def __init__(self, hidden_dims, latent_dims,  optimiser, device='cpu'):
         self.latent_dims = latent_dims
         self.hidden_dims = hidden_dims
         self.state_dims = latent_dims + hidden_dims # st = [ht, zt]
-
+        self.optimiser = optimiser
         self.GRU = nn.GRU(input_size=latent_dims, hidden_size=hidden_dims, device=device)
         self.encoder = Encoder()
         self.decoder = Decoder()
-        self.actor = Actor()
-        self.critic = Critic()
     
+    def forward(self, observation, last_hidden):
+        latent_state = self.encoder(observation)
+        hidden_state = self.GRU(last_hidden)
+        state = torch.cat(hidden_state, latent_state, dim=-1)
+        return state, hidden_state
+    
+    def train(self):
+
