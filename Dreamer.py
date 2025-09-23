8ad1bb200f99a0f3abe9bfff55ecb58e31a681d6
youngers2006 <sy2224@ic.ac.uk>
2025-09-23 17:25:40 +0100

    added

--- Dreamer.py
+++ Dreamer.py
@@ -9,11 +9,3 @@ class Dreamer(nn.Module):
         self.actor = Actor()
         self.critic = Critic()
     
-    def imagine_episode(self):
-        hidden_state = 0
-        latent_state = self.dynamics_predictor(hidden_state)
-        while self.continue_predictor(hidden_state, latent_state):
-            last_hidden_state = hidden_state
-            last_latent_state = latent_state
-        
-        return 0
--- WorldModel.py
+++ WorldModel.py
@@ -18,5 +18,10 @@ class WorldModel(nn.Module):
         self.continue_predictor = ContinuePredictor()
         self.decoder = Decoder()
     
-        
+        def imagine_step(self, last_hidden_state, last_latent_state, action):
+            hidden_state = self.sequence_model(last_hidden_state, last_latent_state, action)
+            latent_state = self.dynamics_predictor.predict(hidden_state)
+            reward = self.reward_predictor(hidden_state, latent_state)
+            continue_ = self.continue_predictor(hidden_state, latent_state)
+            return hidden_state, latent_state, reward, continue_
 
