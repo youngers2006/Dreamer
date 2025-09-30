ff87a609d886633e46811b096225b3d46990cef5
youngers2006 <sy2224@ic.ac.uk>
2025-09-30 19:56:34 +0100

    cleaned up

--- VariationalAutoEncoder.py
+++ VariationalAutoEncoder.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 class Encoder(nn.Module):
-    def __init__(self, latent_num_rows, latent_num_columns, num_filters_1, num_filters_2, hidden_layer_nodes, device='cpu'):
+    def __init__(self, hidden_state_dim, latent_num_rows, latent_num_columns, num_filters_1, num_filters_2, hidden_layer_nodes, device='cpu'):
         """
         Takes obseravtion (image in this class) and maps it to a latent state representation through a CNN.
         """ 
@@ -16,20 +16,22 @@ class Encoder(nn.Module):
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Conv2d(in_channels=num_filters_1, out_channels=num_filters_2, kernel_size=3, stride=1, padding=1, device=device),
             nn.SiLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.AdaptiveAvgPool2d((2, 2))
         )
+        flattened_feature_size = num_filters_2 * 2 * 2
+        total_in_features = flattened_feature_size + hidden_state_dim
+        self.flatten = nn.Flatten()
         self.latent_mapper = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(in_features=num_filters_2 * 2 * 2, out_features=hidden_layer_nodes, device=device),
+            nn.Linear(in_features=total_in_features, out_features=hidden_layer_nodes, device=device),
             nn.SiLU(),
             nn.Linear(in_features=hidden_layer_nodes, out_features=self.latent_size, device=device)
         )
 
-    def forward(self, x1, x2):
-        features = self.feature_extractor(x2)
-        _, _, H, W = features.shape
-        x1 = x1.unsqueeze(-1).unsqueeze(-1).repeat(1, 1, H, W)
-        input = torch.cat((features, x1), axis=1)
+    def forward(self, hidden, latent):
+        features = self.feature_extractor(latent)
+        features = self.flatten(features)
+        input = torch.cat((features, hidden), dim=-1)
         logits = self.latent_mapper(input)
         return logits
     
@@ -38,7 +40,7 @@ class Encoder(nn.Module):
         dist = torch.distributions.Categorical(logits=logits)
         sampled_idx = dist.sample()
         latent_state_flat = torch.nn.functional.one_hot(sampled_idx, num_classes=self.latent_size)
-        latent_state = latent_state_flat.view(-1, self.latent_num_rows, self.latent_num_columns, dtype=torch.float32)
+        latent_state = latent_state_flat.view(-1, self.latent_num_rows, self.latent_num_columns)
         return latent_state, logits
 
 class Decoder(nn.Module):
@@ -47,26 +49,27 @@ class Decoder(nn.Module):
     """
     def __init__(self, latent_num_rows, latent_num_columns, observation_dim, hidden_state_dim, num_filters_1, num_filters_2, hidden_layer_nodes, device='cpu'):
         super().__init__()
-        self.upscale_starting_dim = observation_dim / 4
+        self.upscale_starting_dim = observation_dim[0] // 4
         self.num_filters_2 = num_filters_2
+
         self.flatten = nn.Flatten()
         self.upscaler = nn.Sequential(
-            nn.Linear(in_features=latent_num_rows * latent_num_columns + hidden_state_dim, out_features=hidden_layer_nodes),
+            nn.Linear(in_features=latent_num_rows * latent_num_columns + hidden_state_dim, out_features=hidden_layer_nodes, device=device),
             nn.SiLU(),
-            nn.Linear(in_features=hidden_layer_nodes, out_features=num_filters_2 * self.upscale_starting_dim * self.upscale_starting_dim),
+            nn.Linear(in_features=hidden_layer_nodes, out_features=num_filters_2 * self.upscale_starting_dim * self.upscale_starting_dim, device=device),
             nn.SiLU()
         )
         self.image_builder = nn.Sequential(
-            nn.ConvTranspose2d(in_channels=num_filters_2, out_channels=num_filters_1, kernel_size=4, stride=2, padding=1),
+            nn.ConvTranspose2d(in_channels=num_filters_2, out_channels=num_filters_1, kernel_size=4, stride=2, padding=1, device=device),
             nn.SiLU(),
-            nn.ConvTranspose2d(in_channels=num_filters_1, out_channels=6, kernel_size=4, stride=2, padding=1)
+            nn.ConvTranspose2d(in_channels=num_filters_1, out_channels=6, kernel_size=4, stride=2, padding=1, device=device)
         )
         self.softplus = nn.Softplus()
 
-    def forward(self, x1: torch.tensor, x2: torch.tensor):
-        x2 = self.flatten(x2)
-        x = torch.cat((x1, x2), dim=-1)
-        x = self.upscaler(x)
+    def forward(self, hidden: torch.tensor, latent: torch.tensor):
+        latent = self.flatten(latent)
+        input = torch.cat((hidden, latent), dim=-1)
+        x = self.upscaler(input)
         x = x.view(-1, self.num_filters_2, self.upscale_starting_dim, self.upscale_starting_dim)
         obs_params = self.image_builder(x)
         mu, sigma_logits = torch.chunk(obs_params, chunks=2, dim=1)
@@ -78,6 +81,3 @@ class Decoder(nn.Module):
         dist = torch.distributions.Independent(torch.distributions.Normal(loc=mu, scale=sigma), 3)
         observation = dist.rsample()
         return observation, mu, sigma
-
-
-    
