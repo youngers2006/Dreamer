Fixed all dimensioning errors so now runs correctly, need to implement numerical stability features to avoid exploding gradients which have now appeared in testing

Agent.py
@@ -70,56 +70,54 @@ class Agent(nn.Module): # batched sequence (batch_size, sequence_length, feature
                 continue_batch_seq.shape[1]
             )
         with torch.no_grad():
-            value_batched_seq = self.critic.value(z_batch_seq, h_batch_seq)
-            advantage_batched_seq = R_lambda_batch_seq - value_batched_seq
+            value_batched_seq = self.critic.value(h_batch_seq, z_batch_seq)
+            advantage_batched_seq = R_lambda_batch_seq - value_batched_seq[:, :-1]
+            advantage_batched_seq = advantage_batched_seq.squeeze(-1)
 
         a_dist_batch_seq = Normal(loc=a_mu_batch_seq, scale=a_sigma_batch_seq)
-        log_prob_batch_seq = a_dist_batch_seq.log_prob(action_batch_seq)
-        actor_entropy_batched_seq = a_dist_batch_seq.entropy()
+        log_prob_batch_seq = a_dist_batch_seq.log_prob(action_batch_seq).sum(dim=-1)
+        log_prob_batch_seq = log_prob_batch_seq[:, :-1]
+        actor_entropy_batched_seq = a_dist_batch_seq.entropy().sum(dim=-1)
+        actor_entropy_batched_seq = actor_entropy_batched_seq[:, :-1]
         self.update_S(R_lambda_batch_seq)
         normalisation_term = torch.max(self.S, torch.tensor(1.0, dtype=torch.float32, device=self.device))
         loss_batched_seq_actor = log_prob_batch_seq * (advantage_batched_seq / normalisation_term) + self.nu * actor_entropy_batched_seq
-        loss_actor = -torch.sum(loss_batched_seq_actor, keepdim=False, dim=1).mean()
-
-        critic_logits = self.critic(h_batch_seq, z_batch_seq)
-        R_lambda_th_batch_seq = to_twohot(R_lambda_batch_seq, self.buckets)
+        loss_actor = -torch.mean(loss_batched_seq_actor)
+        critic_logits = self.critic(h_batch_seq, z_batch_seq)[:, :-1]
+        R_lambda_th_batch_seq = to_twohot(R_lambda_batch_seq, self.critic.buckets_crit)
         value_log_probs = nn.functional.log_softmax(critic_logits, dim=-1)
         loss_batched_seq_critic = -torch.sum(R_lambda_th_batch_seq * value_log_probs, dim=-1)
         loss_critic = torch.mean(loss_batched_seq_critic)
 
         self.actor_optimiser.zero_grad()
-        loss_actor.backward()
-        self.actor_optimiser.step()
-
         self.critic_optimiser.zero_grad()
+
+        loss_actor.backward(retain_graph=True)
         loss_critic.backward()
+
+        self.actor_optimiser.step()
         self.critic_optimiser.step()
         return loss_actor, loss_critic
     
     def compute_batched_R_lambda_returns(self, hidden_state_batched_seq, latent_state_batched_seq, reward_batched_seq, continue_batched_seq, seq_length):
-        def compute_R_lambda_returns(hidden_state_seq, latent_state_seq, reward_seq, continue_seq, seq_length):
-            with torch.no_grad():
-                value_estimate_seq = self.critic.value(hidden_state_seq, latent_state_seq).detach()
-            R_lambda_seq = [value_estimate_seq[-1]]
-            for t in reversed(range(seq_length-1)):
-                R_lambda = reward_seq[t] + self.gamma * continue_seq[t] * ((1 - self.lambda_) * value_estimate_seq[t+1] + self.lambda_ * R_lambda_seq[0])
-                R_lambda_seq.insert(0, R_lambda)
-            R_lambda_seq = torch.stack(R_lambda_seq, dim=0)
-            return R_lambda_seq
-        
-        R_lambda_batched_seq = torch.vmap(compute_R_lambda_returns, in_dims=(0,0,0,0,None))(
-            hidden_state_batched_seq, 
-            latent_state_batched_seq, 
-            reward_batched_seq, 
-            continue_batched_seq, 
-            seq_length
-        )
-        return R_lambda_batched_seq
+        with torch.no_grad():
+            value_estimate_seq = self.critic.value(hidden_state_batched_seq, latent_state_batched_seq)
+        next_return = value_estimate_seq[:, -1]
+        R_lambda_seq = []
+        for t in reversed(range(seq_length - 1)):
+            reward_t = reward_batched_seq[:, t]         
+            continue_t = continue_batched_seq[:, t]  
+            value_t_plus_1 = value_estimate_seq[:, t+1] 
+            R_lambda = reward_t + self.gamma * continue_t * (1 - self.lambda_) * value_t_plus_1 + self.lambda_ * next_return
+            R_lambda_seq.insert(0, R_lambda)
+            next_return = R_lambda
+        R_lambda_seq = torch.stack(R_lambda_seq, dim=1)
+        return R_lambda_seq.squeeze(2).squeeze(-1)
         
 class Actor(nn.Module):
     def __init__(self, action_dim, latent_column_dim, latent_row_dim, hidden_state_dim, hidden_layer_num_nodes_1, hidden_layer_num_nodes_2,*, device='cpu'):
         super().__init__()
-        self.flatten = nn.Flatten()
+        self.flatten = nn.Flatten(start_dim=2)
         self.base_net = nn.Sequential(
             nn.Linear(in_features=latent_row_dim * latent_column_dim + hidden_state_dim, out_features=hidden_layer_num_nodes_1, device=device),
             nn.SiLU(),
@@ -140,8 +138,8 @@ class Actor(nn.Module):
     
     def act(self, ht, zt):
         mu, sigma = self.forward(ht, zt)
-        action_dist = Normal(loc=mu, scale=sigma)
-        action = action_dist.rsample()
+        eps = torch.randn_like(mu)
+        action = mu + eps * sigma
         return action, mu, sigma
 
 class Critic(nn.Module):
@@ -150,7 +148,7 @@ class Critic(nn.Module):
         self.latent_row_dim = latent_row_dim
         self.latent_column_dim = latent_column_dim
         self.num_buckets = num_buckets
-        self.flatten = nn.Flatten()
+        self.flatten = nn.Flatten(start_dim=2)
         self.value_net = nn.Sequential(
             nn.Linear(in_features=latent_column_dim * latent_row_dim + hidden_state_dim, out_features=hidden_layer_num_nodes_1, device=device),
             nn.SiLU(),
@@ -171,4 +169,4 @@ class Critic(nn.Module):
         logits = self.forward(ht, zt)
         probs = torch.nn.functional.softmax(logits, dim=-1)
         value = torch.sum(probs * self.buckets_crit, dim=-1, keepdim=True)
-        return value
+        return value 

Dreamer.py
@@ -133,29 +133,38 @@ class Dreamer(nn.Module):
 
     def dream_episodes(self, starting_latent_state_batch, starting_hidden_state_batch):
         def dream_episode(starting_latent_state, starting_hidden_state):
-            hidden_states = [starting_hidden_state]
-            latent_states = [starting_latent_state]
+            hidden_state = starting_hidden_state.unsqueeze(0)
+            latent_state = starting_latent_state.unsqueeze(0)
+            hidden_states = []
+            latent_states = []
             rewards = []
             actions = []
             continues_ = []
             a_mus = []
             a_sigmas = []
-            hidden_state = starting_hidden_state ; latent_state = starting_latent_state
             for _ in range(self.horizon):
                 action, a_mu, a_sigma = self.agent.actor.act(hidden_state, latent_state)
                 hidden_state_, latent_state_, reward, continue_ = self.world_model.imagine_step(hidden_state, latent_state, action)
-                hidden_states.append(hidden_state) ; latent_states.append(latent_state)
+                hidden_states.append(hidden_state.squeeze(1)) ; latent_states.append(latent_state.squeeze(1))
                 rewards.append(reward) ; actions.append(action) ; continues_.append(continue_)
                 a_mus.append(a_mu) ; a_sigmas.append(a_sigma)
                 hidden_state = hidden_state_ ; latent_state = latent_state_
-            latent_states = torch.stack(latent_states[:-1], dim=0)
-            hidden_states = torch.stack(hidden_states[:-1], dim=0)
-            actions = torch.stack(actions, dim=0)
-            rewards = torch.stack(rewards, dim=0)
-            continues_ = torch.stack(continues_, dim=0)
-            a_mus = torch.stack(a_mus, dim=0)
-            a_sigmas = torch.stack(a_sigmas, dim=0)
-            return latent_states, hidden_states, actions, rewards, continues_, a_mus, a_sigmas
+            latent_states = torch.stack(latent_states, dim=1)
+            hidden_states = torch.stack(hidden_states, dim=1)
+            actions = torch.stack(actions, dim=1)
+            rewards = torch.stack(rewards, dim=1)
+            continues_ = torch.stack(continues_, dim=1)
+            a_mus = torch.stack(a_mus, dim=1)
+            a_sigmas = torch.stack(a_sigmas, dim=1)
+            return (
+                latent_states.squeeze(0), 
+                hidden_states.squeeze(0), 
+                actions.squeeze(0), 
+                rewards.squeeze(0), 
+                continues_.squeeze(0), 
+                a_mus.squeeze(0), 
+                a_sigmas.squeeze(0)
+            )
     
         dream_batch_fn = torch.vmap(dream_episode, in_dims=(0,0), randomness='different')
         z_batch_seq, h_batch_seq, a_batch_seq, rewards_batch_seq, continues_batch_seq, a_mu_batch_seq, a_sigma_batch_seq = dream_batch_fn(
@@ -211,15 +220,15 @@ class Dreamer(nn.Module):
         return loss_list
 
     def warm_start_generator(self, observation_seq_batch, action_seq_batch, sequence_length):
-        hidden_batch = torch.zeros(self.batch_size, self.hidden_state_dims, dtype=torch.float32, device=self.device)
-        latent_batch, _ = self.world_model.encoder.encode(hidden_batch, observation_seq_batch[:, 0, :])
+        hidden_batch = torch.zeros(self.batch_size, 1, self.hidden_state_dims, dtype=torch.float32, device=self.device)
+        latent_batch, _ = self.world_model.encoder.encode(hidden_batch, observation_seq_batch[:, 0:1, :])
         warmup_length = sequence_length // 2
         for t in range(1, warmup_length):
             latent_batch, hidden_batch, _ = self.world_model.observe_step(
                 latent_batch, 
                 hidden_batch, 
-                action_seq_batch[:,t-1,:], 
-                observation_seq_batch[:, t, :]
+                action_seq_batch[:,t-1:t,:], 
+                observation_seq_batch[:, t:t+1, :]
             )
         return latent_batch, hidden_batch
     
@@ -259,33 +268,33 @@ class Dreamer(nn.Module):
             self.seed += 1
             total_reward = 0
             observation, _ = env.reset(seed=self.seed)
-            observation = observation.transpose(2,0,1)[np.newaxis, :].astype(np.uint8)
-            observation_tensor = torch.tensor(observation, dtype=torch.float32, device=self.device)
+            observation = observation.transpose(2,0,1).astype(np.uint8)
+            observation_tensor = torch.tensor(observation, dtype=torch.float32, device=self.device).unsqueeze(0).unsqueeze(0)
             continue_ = True
-            hidden_state = torch.zeros(self.hidden_state_dims, dtype=torch.float32, device=self.device)
+            hidden_state = torch.zeros(self.hidden_state_dims, dtype=torch.float32, device=self.device).unsqueeze(0).unsqueeze(0)
             latent_state, _ = self.world_model.encoder.encode(hidden_state, observation_tensor)
             while continue_:
                 action, _, _ = self.agent.actor.act(hidden_state, latent_state)
-                action_np = action.detach().cpu().numpy().squeeze()
+                action_np = action.detach().cpu().numpy().squeeze(0).squeeze(0)
                 observation_, reward, terminated, truncated, _ = env.step(action_np)
-                observation_ = observation_.transpose(2,0,1)[np.newaxis, :].astype(np.uint8)
-                observation__tensor = torch.tensor(observation_, dtype=torch.float32, device=self.device)
+                observation_ = observation_.transpose(2,0,1).astype(np.uint8)
+                observation__tensor = torch.tensor(observation_, dtype=torch.float32, device=self.device).unsqueeze(0).unsqueeze(0)
                 total_reward += reward
                 done = (terminated or truncated)
                 continue_ = (1 - done)
-                hidden_state, latent_state, _ = self.world_model.observe_step(latent_state, hidden_state, action, observation__tensor)
+                latent_state, hidden_state, _ = self.world_model.observe_step(latent_state, hidden_state, action, observation__tensor)
                 observation = observation_
                 observation_tensor = observation__tensor
             reward_list.append(total_reward)
-        reward_list = torch.stack(reward_list, dim=0)
-        return reward_list.mean(dim=0)
+        reward_list = torch.tensor(reward_list, dtype=torch.float32, device=self.device)
+        return reward_list.mean()
     
     def train_dreamer(self, env, eval_env):
         WM_loss_list = [] ; actor_loss_list = [] ; critic_loss_list = []
         evaluation_list = []
         print("Starting Training...")
         print("Starting Random Kickstart.")
-        for _ in tqdm(range(self.random_iterations), desc="Kickstarting Dreamer Agent.", leave=False):
+        for _ in tqdm(range(self.random_iterations), desc=f"Kickstarting Dreamer Agent.", leave=True):
             self.rollout_policy(env, random_policy=True)
             WM_loss = self.train_world_model()
             actor_loss, critic_loss = self.train_Agent()

DreamerUtils.py
@@ -8,7 +8,9 @@ def gaussian_log_probability(x: torch.tensor, mu: torch.tensor, sigma: torch.ten
     return log_prob
 
 def bernoulli_log_probability(p, k):
-    log_prob = k * torch.log(p) + (1 - k) * torch.log(1 - p)
+    epsilon = 1e-8
+    p_clamped = torch.clamp(p, min=epsilon, max=1.0 - epsilon)
+    log_prob = k * torch.log(p_clamped) + (1 - k) * torch.log(1 - p_clamped)
     return log_prob
 
 def kullback_leibler_divergence_between_gaussians(
@@ -30,12 +32,12 @@ def symexp(x):
 
 def to_twohot(value: torch.tensor, buckets: torch.tensor):
     clipped_value = torch.clamp(max=buckets.max(), min=buckets.min(), input=value)
-    lower_bucket_idx = torch.sum((buckets <= clipped_value), dim=-1, keepdim=True) - 1
+    lower_bucket_idx = torch.searchsorted(buckets, clipped_value, right=True) - 1
     lower_bucket_val = buckets[lower_bucket_idx]
     upper_bucket_val = buckets[lower_bucket_idx + 1]
     weight = (clipped_value - lower_bucket_val) / (upper_bucket_val - lower_bucket_val + 1e-8)
-    twohot_shape = list(value.shape) + [buckets.shape[-1]]
+    twohot_shape = value.shape[:-1] + (buckets.shape[0],)
     twohot = torch.zeros(twohot_shape, dtype=torch.float32, device=value.device)
-    twohot = torch.scatter(twohot, dim=-1, index=lower_bucket_idx.unsqueeze(-1), src=(1.0 - weight).unsqueeze(-1))
-    twohot = torch.scatter(twohot, dim=-1, index=(lower_bucket_idx + 1).unsqueeze(-1), src=weight.unsqueeze(-1))
+    twohot = torch.scatter(twohot, dim=-1, index=lower_bucket_idx, src=(1.0 - weight))
+    twohot = torch.scatter(twohot, dim=-1, index=(lower_bucket_idx + 1), src=weight)
     return twohot

WorldModel.py
@@ -62,7 +62,7 @@ class WorldModel(nn.Module):
         self.optimiser = torch.optim.AdamW(
             self.parameters(), 
             lr=WM_lr, 
-            betas=(WM_betas[0], WM_betas[1]), 
+            betas=(WM_betas[0], WM_betas[1]),  
             eps=WM_eps
         )
 