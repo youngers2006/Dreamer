c8b9c02e4b3c2d931cf3e7703348d879821148a9
youngers2006 <sy2224@ic.ac.uk>
2025-12-05 06:02:42 +0000

    changes

--- Adaptors.py
+++ Adaptors.py
@@ -4,6 +4,7 @@ import gymnasium as gym
 class CarRacerAdaptor(gym.ActionWrapper):
     def __init__(self, env):
         super().__init__(env)
+        self.action_space = gym.spaces.Box(low=-1, high=1, shape=(3,), dtype=np.float32)
 
     def action(self, action):
         steering = action[0]
