aaa7156f86a14015a7dc32c2bd5b51271e224946
youngers2006 <sy2224@ic.ac.uk>
2026-03-20 18:26:33 +0000

    added doc string and cleaned code

--- Networks/DynamicsPredictors.py
+++ Networks/DynamicsPredictors.py
@@ -5,15 +5,37 @@ import torch.nn as nn
 from Utils import symexp
 
 class DynamicsPredictor(nn.Module):
+    """Next latent state predictor network section of the Recurrent State Space Model (RSSM).
+
+    This class is a FF-network that outputs the latent state for the current timestep
+    given the current hidden state.
+
+    Args:
+        latent_num_rows (int): Number of rows in the discrete latent state matrix.
+        latent_num_columns (int): Number of columns in the discrete latent state matrix.
+        hidden_state_size (int): Size of hidden state vector.
+        hidden_L1 (int): Number of neurons in the first hidden state of the network.
+        hidden_L2 (int): Number of neurons in the second hidden state of the network.
+        device (str, optional): Storage location of the network ('cpu' or 'cuda'). 
+            Defaults to 'cpu'.
+
+    Attributes:
+        latent_num_rows (int): Number of rows in the latent state matrix.
+        latent_num_columns (int): Number of columns in the latent state matrix.
+        latent_size (int): Size of flattened latent state matrix.
+        device (str): Storage location of the network ('cpu' or 'cuda').
     """
-    Takes the current hidden state of the sequence model and predicts the encoded state
-    """
-    def __init__(self, latent_num_rows, latent_num_columns, hidden_state_size, hidden_L1, hidden_L2, device):
+    def __init__(
+            self, latent_num_rows: int, latent_num_columns: int,
+            hidden_state_size: int, hidden_L1: int, hidden_L2: int,
+            *, device='cpu'
+        ):
         super().__init__()
         self.latent_num_rows = latent_num_rows
         self.latent_num_columns = latent_num_columns
         self.latent_size = latent_num_rows * latent_num_columns
         self.device = device
+
         self.logit_net = nn.Sequential(
             nn.Linear(in_features=hidden_state_size, out_features=hidden_L1, device=device),
             nn.LayerNorm(hidden_L1, device=device),
@@ -24,84 +46,241 @@ class DynamicsPredictor(nn.Module):
             nn.Linear(in_features=hidden_L2, out_features=self.latent_size, device=device)
         )
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor):
+        """Runs network forward pass to compute prediction logits.
+
+        This function runs a forward pass of the dynamics networks to compute prediction
+        logits for the input x.
+
+        Args:
+            x (torch.Tensor): Network input.
+
+        Returns:
+            logits (torch.Tensor): Prediction logits.
+        """
+        # Run forward pass to compute logits
         logits = self.logit_net(x)
-        B, S, _ = logits.shape
-        logits = logits.view(B, S, self.latent_num_rows, self.latent_num_columns)
+
+        # resize logits tensor in feature dimensions to recover matrix and return logits
+        batch_dim, seq_dim, _ = logits.shape
+        logits = logits.view(
+            batch_dim, seq_dim, self.latent_num_rows, self.latent_num_columns
+        )
         return logits
-    
-    def predict(self, hidden_state: torch.tensor):
+   
+    def predict(self, hidden_state: torch.Tensor):
+        """Runs dynamics predictor to compute latent state from hidden state.
+
+        This function computes the stochastic discrete latent state given the hidden state.
+
+        Args:
+            hidden_state (torch.Tensor): Current hidden state, h_t.
+
+        Returns:
+            latent_state: Current latent state, z_t.
+            logits: Prediction logits of the network, used for training.
+        """
+        # Run forward pass to compute prediction logits
         logits = self.forward(hidden_state)
+
+        # Compute probability distribution of latent states and sample from it
         probs = torch.softmax(logits, dim=-1)
-        uniform = (1.0 / self.latent_num_columns)
+        uniform = 1.0 / self.latent_num_columns
         probs = 0.99 * probs + 0.01 * uniform
         dist = torch.distributions.Categorical(probs=probs)
         sample_idx = dist.sample()
-        latent_state_OH = torch.nn.functional.one_hot(sample_idx, num_classes=self.latent_num_columns).float()
-        latent_state = latent_state_OH + probs - probs.detach()
+
+        # Construct latent state matrix with onehot encodings and use STE to allow gradient flow
+        latent_state_one_hot = torch.nn.functional.one_hot(
+            sample_idx, num_classes=self.latent_num_columns
+        ).float()
+        latent_state = latent_state_one_hot + probs - probs.detach()
         return latent_state, logits
-    
+
 class RewardPredictor(nn.Module):
-    """ 
-    Takes the current hidden state of the sequence model and the latent state and predicts the reward
+    """Reward predictor network.
+
+    This class is a FF-network that outputs the predicted reward for the current state.
+
+    Args:
+        latent_num_rows (int): Number of rows in the discrete latent state matrix.
+        latent_num_columns (int): Number of columns in the discrete latent state matrix.
+        hidden_state_size (int): Size of hidden state vector.
+        hidden_L1 (int): Number of neurons in the first hidden state of the network.
+        hidden_L2 (int): Number of neurons in the second hidden state of the network.
+        num_buckets (int): Number of reward buckets available.
+        device (str, optional): Storage location of the network ('cpu' or 'cuda'). 
+            Defaults to 'cpu'.
+
+    Attributes:
+        latent_num_rows (int): Number of rows in the latent state matrix.
+        latent_num_columns (int): Number of columns in the latent state matrix.
+        latent_size (int): Size of flattened latent state matrix.
+        device (str): Storage location of the network ('cpu' or 'cuda').
     """
-    def __init__(self, latent_num_rows, latent_num_columns, hidden_state_size, hidden_L1, hidden_L2, num_buckets=255, device='cpu'):
+    def __init__(
+            self, latent_num_rows: int, latent_num_columns: int,
+            hidden_state_size: int, hidden_L1: int, hidden_L2: int,
+            num_buckets=255, device='cpu'
+        ):
         super().__init__()
         self.latent_size = latent_num_rows * latent_num_columns
         self.buckets = num_buckets
         self.device = device
+
         self.flatten = nn.Flatten(start_dim=2)
         self.logit_net = nn.Sequential(
-            nn.Linear(in_features=hidden_state_size + self.latent_size, out_features=hidden_L1, device=device),
+            nn.Linear(
+                in_features=hidden_state_size + self.latent_size,
+                out_features=hidden_L1, device=device
+            ),
             nn.LayerNorm(hidden_L1, device=device),
             nn.SiLU(),
-            nn.Linear(in_features=hidden_L1, out_features=hidden_L2, device=device),
+            nn.Linear(
+                in_features=hidden_L1, out_features=hidden_L2,
+                device=device
+            ),
             nn.LayerNorm(hidden_L2, device=device),
             nn.SiLU(),
-            nn.Linear(in_features=hidden_L2, out_features=num_buckets, device=device)
+            nn.Linear(
+                in_features=hidden_L2, out_features=num_buckets,
+                device=device
+            )
         )
-        buckets = torch.linspace(-20.0, 20.0, num_buckets, device=device)
+
+        # Setup reward buckets to allow
+        buckets = torch.linspace(
+            -20.0, 20.0, num_buckets, device=device
+        )
         self.register_buffer('buckets_rew', buckets)
-    
-    def forward(self, hidden, latent):
+
+    def forward(self, hidden: torch.Tensor, latent: torch.Tensor):
+        """Runs network forward pass to compute prediction logits.
+
+        This function runs a forward pass of the reward network, computes reward logits
+        for each reward bucket.
+
+        Args:
+            hidden (torch.Tensor): Current hidden state, h_t.
+            latent (torch.Tensor): Current latent state, z_t.
+
+        Returns:
+            logits (torch.Tensor): Prediction logits.
+        """
+
+        # Concatenate hidden and flattned latent states along the feature dims
         latent = self.flatten(latent)
         input = torch.cat([hidden, latent], dim=-1)
+
+        # Compute reward logits
         logits = self.logit_net(input)
         return logits
-    
-    def predict(self, hidden_state: torch.tensor, latent_state: torch.tensor):
+
+    def predict(self, hidden_state: torch.Tensor, latent_state: torch.Tensor):
+        """Runs reward predictor to compute latent state from hidden state.
+
+        This function computes the stochastic discrete latent state given the hidden state.
+
+        Args:
+            hidden_state (torch.Tensor): Current hidden state, h_t.
+            latent_state (torch.Tensor): Current latent state, z_t.
+
+        Returns:
+            reward (torch.Tensor): Predicted reward buckets.
+        """
+
+        # Compute symlog reward buckets
         logits = self.forward(hidden_state, latent_state)
         probs = torch.nn.functional.softmax(logits, dim=-1)
         symlog_reward = torch.sum(probs * self.buckets_rew, dim=-1, keepdim=True)
-        return symexp(symlog_reward)
-    
+
+        # Convert symlog rewards back to reward buckets and return
+        reward = symexp(symlog_reward)
+        return reward
+
 class ContinuePredictor(nn.Module):
+    """Reward predictor network.
+
+    This class is a FF-network that outputs the predicted reward for the current state.
+
+    Args:
+        latent_num_rows (int): Number of rows in the discrete latent state matrix.
+        latent_num_columns (int): Number of columns in the discrete latent state matrix.
+        hidden_state_size (int): Size of hidden state vector.
+        hidden_L1 (int): Number of neurons in the first hidden state of the network.
+        hidden_L2 (int): Number of neurons in the second hidden state of the network.
+        device (str, optional): Storage location of the network ('cpu' or 'cuda'). 
+            Defaults to 'cpu'.
+
+    Attributes:
+        latent_size (int): Size of flattened latent state matrix.
+        device (str): Storage location of the network ('cpu' or 'cuda').
     """
-    Takes the current hidden state of the sequence model and the latent state and predicts if imagined predicted episode should continue
-    """
-    def __init__(self, latent_num_rows, latent_num_columns, hidden_state_size, hidden_L1, hidden_L2, device):
+    def __init__(
+            self, latent_num_rows: int, latent_num_columns: int,
+            hidden_state_size: int, hidden_L1: int, hidden_L2: int,
+            *, device='cpu'
+        ):
         super().__init__()
         self.latent_size = latent_num_rows * latent_num_columns
         self.device = device
+
         self.flatten = nn.Flatten(start_dim=2)
         self.logit_generator = nn.Sequential(
-            nn.Linear(in_features=hidden_state_size + self.latent_size, out_features=hidden_L1, device=device),
+            nn.Linear(
+                in_features=hidden_state_size + self.latent_size, 
+                out_features=hidden_L1, device=device
+            ),
             nn.LayerNorm(hidden_L1, device=device),
             nn.SiLU(),
-            nn.Linear(in_features=hidden_L1, out_features=hidden_L2, device=device),
+            nn.Linear(
+                in_features=hidden_L1, out_features=hidden_L2,
+                device=device
+            ),
             nn.LayerNorm(hidden_L2, device=device),
             nn.SiLU(),
-            nn.Linear(in_features=hidden_L2, out_features=1, device=device)
+            nn.Linear(
+                in_features=hidden_L2, out_features=1, device=device
+            )
         )
 
-    def forward(self, hidden, latent):
+    def forward(self, hidden: torch.Tensor, latent: torch.Tensor):
+        """Runs network forward pass to compute continue probability.
+
+        This function runs a forward pass of the continue network, 
+        computes continue probability.
+
+        Args:
+            hidden (torch.Tensor): Current hidden state, h_t.
+            latent (torch.Tensor): Current latent state, z_t.
+
+        Returns:
+            probability (torch.Tensor): Probability of continuing.
+            logits (torch.Tensor): Prediction logits, for training.
+        """
+
+        # concat flattened latent state and hidden state along feature dim
         latent = self.flatten(latent)
-        input = torch.cat([hidden, latent], dim=-1)
-        logit = self.logit_generator(input)
+        net_input = torch.cat([hidden, latent], dim=-1)
+
+        # Computes probability of continuation
+        logit = self.logit_generator(net_input)
         probability = torch.sigmoid(logit)
         return probability, logit
-    
-    def predict(self, hidden_state: torch.tensor, latent_state: torch.tensor):
+
+    def predict(self, hidden_state: torch.Tensor, latent_state: torch.Tensor):
+        """Computes continue probability.
+
+        This function computes continue probability.
+
+        Args:
+            hidden_state (torch.Tensor): Current hidden state, h_t.
+            latent_state (torch.Tensor): Current latent state, z_t.
+
+        Returns:
+            probability (torch.Tensor): Probability of continuing, continue_ = (probability >= 0.5).
+        """
+
+        # Run forward pass and return output
         probability, _ = self.forward(hidden_state, latent_state)
-        #continue_ = (probability >= 0.5)
         return probability
--- Networks/SequenceModel.py
+++ Networks/SequenceModel.py
@@ -2,7 +2,29 @@ import torch
 import torch.nn as nn
 
 class SequenceModel(nn.Module):
-    def __init__(self, latent_num_rows, latent_num_columns, hidden_dim, action_dim, *, num_layers=1, device='cpu'):
+    """Sequence GRU network section of the Recurrent State Space Model (RSSM).
+
+    This class is a GRU network that outputs the hidden state for the next timestep
+    given previous hidden state, previous latent state and previous action taken.
+
+    Args:
+        latent_num_rows (int): Number of rows in the discrete latent state matrix.
+        latent_num_columns (int): Number of columns in the discrete latent state matrix.
+        hidden_dim (int): Size of the GRU hidden state vector.
+        action_dim (int): Size of the action vector.
+        device (str, optional): Storage location of the network ('cpu' or 'cuda'). 
+            Defaults to 'cpu'.
+
+    Attributes:
+        latent_dim (int): Size of flattened discrete latent state matrix.
+        hidden_dim (int): Size of hidden state.
+        num_layers (int): Number of GRU layers.
+        device (str): Storage location of the network ('cpu' or 'cuda').
+    """
+    def __init__(
+            self, latent_num_rows: int, latent_num_columns: int,
+            hidden_dim: int, action_dim: int, *, num_layers=1, device='cpu'
+        ):
         super().__init__()
         self.latent_dim = latent_num_columns * latent_num_rows
         self.hidden_dim = hidden_dim
@@ -16,10 +38,30 @@ class SequenceModel(nn.Module):
             device=device
         )
 
-    def forward(self, last_latent_state: torch.tensor, last_hidden_state: torch.tensor, last_action: torch.tensor):
+    def forward(
+            self, last_latent_state: torch.Tensor,
+            last_hidden_state: torch.Tensor, last_action: torch.Tensor
+        ):
+        """Runs network forward pass to compute next hidden state.
+
+        This function computes the deterministic hidden state for the next timestep
+        given previous hidden state, previous latent state and previous action taken.
+
+        Args:
+            last_latent_state (torch.Tensor): Previous latent state, z_t-1.
+            last_hidden_state (torch.Tensor): Previous hidden state, h_t-1.
+            last_action (torch.Tensor): Previous action, a_t-1.
+
+        Returns:
+            hidden: Next timestep hidden state, h_t.
+        """
+        # Flatten latent matrix into vector in seq and feature dimensions
         last_latent_state = self.flatten(last_latent_state)
+
+        # Concatenate latent state and action along feature dimension and remove sequence dim
         input_tensor = torch.cat((last_latent_state, last_action), dim=-1).squeeze(1)
         last_hidden_state = last_hidden_state.squeeze(1)
+
+        # Compute hidden state and add sequence dimension
         hidden = self.GRU(input_tensor, last_hidden_state)
         return hidden.unsqueeze(1)
-
